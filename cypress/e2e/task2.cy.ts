c3245ae570b3d10da23e6792a19cb541f9f9047a
ilan <ilan.v2703@gmail.com>
2025-03-27 14:43:45 +0200

    feat: add navbar validation and update element selectors for improved reliability

--- cypress/e2e/task2.cy.ts
+++ cypress/e2e/task2.cy.ts
@@ -8,6 +8,7 @@ const customerServicePage = new customerService();
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.amazon.com/')
+    navbar.validateNavBar()
     navbar.clickOnCustomerService()
     customerServicePage.search('where is my stuff')
   })
--- cypress/pages/navbarPage.ts
+++ cypress/pages/navbarPage.ts
@@ -1,13 +1,22 @@
 export class navbarPage {
 
     elements = {
-        all: () => cy.contains('span.hm-icon-label', 'All'),  
-        todaysDeals: () => cy.contains('a', "Today's Deals"),
-        registry: () => cy.contains('a', 'Registry'),
-        customerService: () => cy.contains('a', 'Customer Service'),
-        giftCards: () => cy.contains('a', 'Gift Cards'),
-        sell: () => cy.contains('a', 'Sell')
+        all: () => cy.get('#nav-hamburger-menu'),  
+        todaysDeals: () => cy.get('[data-csa-c-content-id="nav_cs_gb"]'),
+        registry: () => cy.get('[data-csa-c-content-id="nav_cs_registry"]'),
+        customerService: () => cy.get('[data-csa-c-content-id="nav_cs_customerservice"]'),
+        giftCards: () => cy.get('[data-csa-c-content-id="nav_cs_gc"]'),
+        sell: () => cy.get('[data-csa-c-content-id="nav_cs_sell"]')
    }
+
+    validateNavBar() {
+        this.elements.all().should('be.visible').and('not.be.disabled')
+        this.elements.todaysDeals().should('be.visible').and('not.be.disabled')
+        this.elements.registry().should('be.visible').and('not.be.disabled')
+        this.elements.customerService().should('be.visible').and('not.be.disabled')
+        this.elements.giftCards().should('be.visible').and('not.be.disabled')
+        this.elements.sell().should('be.visible').and('not.be.disabled')
+    }
 
     clickOnCustomerService() {
         this.elements.customerService().click()
